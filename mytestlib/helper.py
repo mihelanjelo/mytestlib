bb4d0e75ed4e8b9a462dd8c50b94beda299dfdc8
mihelanjelo <mixanrattle@gmail.com>
2018-10-23 02:53:01 +0300

    added helper

--- mytestlib/helper.py
+++ mytestlib/helper.py
@@ -1,9 +1,19 @@
 from mytestlib.base_page import BasePage
 
 
-driver = None
+class Helper:
+    def __init__(self, driver):
+        self.driver = driver
 
+    def on_page(self, page) -> BasePage:
+        return page(self.driver)
 
-def on(page: str) -> BasePage:
-    return globals()[page](driver)
+    def visit(self, url):
+        self.driver.get(url)
+
+    def quit(self):
+        self.driver.quit()
+
+    def refresh(self):
+        self.driver.refresh()
 
